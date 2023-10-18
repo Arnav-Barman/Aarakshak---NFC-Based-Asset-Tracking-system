efce5ed128dd3eacce16e90e46460c23c7329fe9
Arnav Barman <arnavbarman1@gmail.com>
2023-10-18 20:22:29 +0530

    Added all files via upload

---
+++ Hardware (Arduino codes)/CleanTag.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SPI pn532spi(SPI, 10);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+
+
+void setup(void) {
+    Serial.begin(115200);
+    Serial.println("NFC Tag Cleaner");
+    nfc.begin();
+}
+
+void loop(void) {
+
+    Serial.println("\nPlace a tag on the NFC reader to clean.");
+
+    if (nfc.tagPresent()) {
+
+        bool success = nfc.clean();
+        if (success) {
+            Serial.println("\nSuccess, tag restored to factory state.");
+        } else {
+            Serial.println("\nError, unable to clean tag.");
+        }
+
+    }
+    delay(5000);
+}
